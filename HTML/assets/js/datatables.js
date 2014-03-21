0b6d417e878c452f11937a8114955beba3e50df8
Jeromie Fernandes <jeromie@ajency.in>
2014-03-21 17:40:44 +0530

    builder button styles

--- HTML/assets/js/datatables.js
+++ HTML/assets/js/datatables.js
@@ -229,6 +229,45 @@ $(document).ready(function() {
         }
 	});
 
+var tableElement = $('#students');
+
+    tableElement.dataTable( {
+		"sDom": "<'row'<'col-md-6'l ><'col-md-6'f>r>t<'row'<'col-md-12'p i>>",
+			"oTableTools": {
+			"aButtons": [
+				{
+					"sExtends":    "collection",
+					"sButtonText": "<i class='fa fa-cloud-download'></i>",
+					"aButtons":    [ "csv", "xls", "pdf", "copy"]
+				}
+			]
+		},
+		"aLengthMenu": [ 25, 50, 100 ],
+		"sPaginationType": "bootstrap",
+		 "aoColumnDefs": [
+          { 'bSortable': false, 'aTargets': [  ] }
+		],
+		"aaSorting": [],
+		"oLanguage": {
+			"sLengthMenu": "_MENU_ ",
+			"sInfo": "Showing <b>_START_ to _END_</b> of _TOTAL_ entries"
+		},
+		 bAutoWidth     : false,
+        fnPreDrawCallback: function () {
+            // Initialize the responsive datatables helper once.
+            if (!responsiveHelper) {
+                responsiveHelper = new ResponsiveDatatablesHelper(tableElement, breakpointDefinition);
+            }
+        },
+        fnRowCallback  : function (nRow) {
+            responsiveHelper.createExpandIcon(nRow);
+        },
+        fnDrawCallback : function (oSettings) {
+            responsiveHelper.respond();
+        }
+	});
+
+
 	var tableElement = $('#questionsAll');
 
     tableElement.dataTable( {
