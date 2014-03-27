72a2df79e1e61a460c369ba577b2e8f401261305
Naizy Monteiro <naizy@ajency.in>
2014-03-27 15:32:21 +0530

    pace options

--- wp-content/themes/walnut/walnut/dev/js/plugins/pace.js
+++ wp-content/themes/walnut/walnut/dev/js/plugins/pace.js
@@ -8,8 +8,8 @@
   defaultOptions = {
     catchupTime: 500,
     initialRate: .03,
-    minTime: 500,
-    ghostTime: 0,
+    minTime: 2000,
+    ghostTime: 500,
     maxProgressPerFrame: 10,
     easeFactor: 1.25,
     startOnPageLoad: false,
