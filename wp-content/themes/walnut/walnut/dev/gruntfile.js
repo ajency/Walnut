e26ca0d01748fb88e0138a8e12a2ffbc17947e78
Naizy Monteiro <naizy@ajency.in>
2014-04-02 15:59:47 +0530

    compressed css files into single one version number for css and js

---
+++ wp-content/themes/walnut/walnut/dev/gruntfile.js
@@ -0,0 +1,23 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    cssmin: {
+      combine: {
+        files: {
+          '../css/production/walnut.min.css': ['css/pace.coinspin.css','css/jquery.sidr.light.css','css/bootstrap.min.css',
+          'css/bootstrap-theme.css','css/font-awesome.css','css/animate.min.css',
+          'css/style.css','css/responsive.css','css/custom-icon-set.css','css/custom.css']
+        }
+      }
+    }
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+  // Default task(s).
+  grunt.registerTask('default', ['cssmin']);
+
+};
