e41f4baa4635dbd2d0f71b4f0924730bd5acbc63
Naizy Monteiro <naizy@ajency.in>
2014-04-16 19:20:53 +0530

    user data

--- wp-content/themes/walnut/modules/user/ajax.php
+++ wp-content/themes/walnut/modules/user/ajax.php
@@ -6,11 +6,10 @@ add_action( 'wp_ajax_get-user-data', 'get_user_data' );
 function get_user_data() {
         if(is_user_logged_in()){
             $user_data=get_userdata(get_current_user_id());
-            echo(wp_send_json(array('data'=>$user_data, 'success'=>true)));
+            wp_send_json(array('data'=>$user_data, 'success'=>true));
         }
         else
-            echo(wp_send_json(array("error"=>"User not logged in.")));
-	die;
+           wp_send_json(array("error"=>"User not logged in."));
 }
 
 add_action( 'wp_ajax_logout_user', 'logout_current_user' );
