56c73d8bb0cd79c2ddadb51135f3751b0acd6d55
Naizy Monteiro <naizy@ajency.in>
2014-04-16 18:23:22 +0530

    mobile authentication

--- wp-content/themes/walnut/functions.php
+++ wp-content/themes/walnut/functions.php
@@ -31,17 +31,3 @@ function upload_attachment($file_handler, $post_id, $setthumb = 'false') {
 		update_post_meta($post_id, '_thumbnail_id', $attach_id);
 	return $attach_id;
 }
-
-
-add_action( 'wp_ajax_nopriv_get-user-profile', 'authenticate_login' );
-function authenticate_login() {
-	$login_data=$_POST['data'];
-	$login_check=wp_authenticate($login_data['txtusername'],$login_data['txtpassword']);
-	if(is_wp_error($login_check))
-		echo(json_encode(array("error"=>"Invalid Username or Password")));
-	else{
-		wp_set_auth_cookie( $login_check->ID );
-		echo(json_encode($login_check));
-	}
-	die;
-}
