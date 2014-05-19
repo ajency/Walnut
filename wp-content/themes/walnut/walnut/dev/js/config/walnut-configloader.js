5020b60775e09b1a305a391e82d6b4937bb63f83
Deepak Prabhudesai <deepak@ajency.in>
2014-05-19 10:46:37 +0530

    no message

--- wp-content/themes/walnut/walnut/dev/js/config/walnut-configloader.js
+++ wp-content/themes/walnut/walnut/dev/js/config/walnut-configloader.js
@@ -1,1 +1,1 @@
-define(['config/marionette', 'config/backbone', 'config/jquery-validate', 'config/jquery', 'config/middle-layer', 'config/app-storage', 'config/app-functions'], function() {});
+define(['config/marionette', 'config/backbone', 'config/jquery-validate', 'config/jquery', 'config/middle-layer', 'config/app-storage', 'config/app-functions', 'config/app-sync'], function() {});
