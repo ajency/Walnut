2f4b2b7cec0c3325b0efac932fe694b9955a5817
NikhilAjency <nikhil@ajency.in>
2014-06-13 13:30:40 +0530

    media sync

--- wp-content/themes/walnut/walnut/dev/js/apps/app-sync/app-sync-operations/app-sync-media.js
+++ wp-content/themes/walnut/walnut/dev/js/apps/app-sync/app-sync-operations/app-sync-media.js
@@ -5,34 +5,64 @@ define(['underscore', 'jquery'], function(_, $) {
       $('#syncMediaSuccess').css("display", "block").text("Contacting server...");
       localFiles = _.getListOfMediaFilesFromLocalDirectory();
       return localFiles.done(function(local_entries) {
-        var filesOnServer, filesToBeDownloaded;
+        var filesOnServer;
+        console.log('local_entries');
+        console.log(local_entries);
         filesOnServer = _.getListOfMediaFilesFromServer();
-        filesToBeDownloaded = _.compareFiles(local_entries, filesOnServer);
-        return _.each(filesToBeDownloaded, function(file, i) {
-          var directoryPath, directoryStructure;
-          directoryPath = file.substr(file.indexOf("uploads/"));
-          directoryStructure = _.createDirectoryStructure(directoryPath);
-          return directoryStructure.done(function() {
-            return console.log('Directory successfully created');
+        return filesOnServer.done(function(files_on_server) {
+          var filesToBeDownloaded;
+          filesToBeDownloaded = _.compareFiles(local_entries, files_on_server);
+          return filesToBeDownloaded.done(function(files_to_be_downloaded) {
+            $('#syncMediaSuccess').css("display", "block").text("Downloading files...");
+            return _.each(files_to_be_downloaded, function(file, i) {
+              var directoryPath, directoryStructure, fileName, localPath, uri;
+              directoryPath = file.substr(file.indexOf("uploads/"));
+              fileName = file.substr(file.lastIndexOf('/') + 1);
+              uri = encodeURI(file);
+              localPath = _.getSynapseImagesDirectoryPath() + directoryPath;
+              directoryStructure = _.createDirectoryStructure(directoryPath);
+              return directoryStructure.done(function() {
+                var fileTransfer;
+                fileTransfer = new FileTransfer();
+                return fileTransfer.download(uri, localPath, function(file) {
+                  return $('#syncMediaSuccess').css("display", "block").text("Downloaded file " + fileName);
+                }, function(error) {
+                  $('#syncMediaSuccess').css("display", "none");
+                  return $('#syncMediaError').css("display", "block").text("An error occurred during file download.");
+                }, true);
+              });
+            });
           });
         });
       });
     },
     compareFiles: function(localEntries, serverEntries) {
-      var filesTobeDownloaded;
-      filesTobeDownloaded = [];
-      _.each(serverEntries.fileImg, function(serverFile, i) {
-        var fileName;
-        fileName = serverFile[i].substr(serverFile[i].lastIndexOf('/') + 1);
-        if (localEntries.indexOf(fileName) === -1) {
-          return filesTobeDownloaded.push(serverFile[i]);
-        }
-      });
-      return filesTobeDownloaded;
+      var runFunc;
+      runFunc = function() {
+        return $.Deferred(function(d) {
+          var filesTobeDownloaded;
+          if (localEntries.length === 0) {
+            return d.resolve(serverEntries.fileImg);
+          } else {
+            filesTobeDownloaded = [];
+            _.each(serverEntries.fileImg, function(serverFile, i) {
+              var fileName;
+              fileName = serverFile[i].substr(serverFile[i].lastIndexOf('/') + 1);
+              if (localEntries.indexOf(fileName) === -1) {
+                return filesTobeDownloaded.push(serverFile[i]);
+              }
+            });
+            return d.resolve(filesTobeDownloaded);
+          }
+        });
+      };
+      return $.when(runFunc()).done(function() {
+        return console.log('compareFiles done');
+      }).fail(_.failureHandler);
     },
     getListOfMediaFilesFromLocalDirectory: function() {
-      var listOfPresentFilesInDirectory;
-      listOfPresentFilesInDirectory = function() {
+      var runFunc;
+      runFunc = function() {
         return $.Deferred(function(d) {
           var fullDirectoryEntry;
           fullDirectoryEntry = [];
@@ -46,11 +76,8 @@ define(['underscore', 'jquery'], function(_, $) {
               return reader.readEntries((function(directoryEnrty) {
                 var i, _i, _ref;
                 for (i = _i = 0, _ref = directoryEnrty.length - 1; _i <= _ref; i = _i += 1) {
-                  console.log(directoryEnrty[i].name + ' dir? ' + directoryEnrty[i].isDirectory);
-                  console.log("your path is " + directoryEnrty[i].toURL());
                   fullDirectoryEntry[i] = directoryEnrty[i].name;
                 }
-                console.log("value is " + fullDirectoryEntry);
                 return d.resolve(fullDirectoryEntry);
               }));
             }, function(error) {
@@ -59,17 +86,25 @@ define(['underscore', 'jquery'], function(_, $) {
           }, _.fileSystemErrorHandler);
         });
       };
-      return $.when(listOfPresentFilesInDirectory()).done(function() {
-        return console.log('Got list of all files present in the directory');
+      return $.when(runFunc()).done(function() {
+        return console.log('Got list of all files present in the local directory');
       }).fail(_.failureHandler);
     },
     getListOfMediaFilesFromServer: function() {
-      var listOfFiles;
-      listOfFiles = [];
-      listOfFiles = {
-        fileImg: ["http://synapsedu.info/wp-content/uploads/videos/oceans-clip.mp4", "http://synapsedu.info/wp-content/uploads/2014/05/tux.png", "http://synapsedu.info/wp-content/uploads/2014/05/Vertical-large.jpg", "http://synapsedu.info/wp-content/uploads/2014/05/imag56es.jpg", "http://synapsedu.info/wp-content/uploads/2014/05/girl1.jpg", "http://synapsedu.info/wp-content/uploads/2014/05/tom_jerry.jpg", "http://synapsedu.info/wp-content/uploads/2014/05/cover_pic1.png", "http://synapsedu.info/wp-content/uploads/2014/06/Tulips.jpg", "http://synapsedu.info/wp-content/uploads/2014/06/Jellyfish.jpg", "http://synapsedu.info/wp-content/uploads/2014/06/Koala.jpg", "http://synapsedu.info/wp-content/uploads/2014/06/Lighthouse.jpg"]
+      var runFunc;
+      runFunc = function() {
+        return $.Deferred(function(d) {
+          var listOfFiles;
+          listOfFiles = [];
+          listOfFiles = {
+            fileImg: ["http://synapsedu.info/wp-content/uploads/videos/oceans-clip.mp4", "http://synapsedu.info/wp-content/uploads/2014/05/tux.png", "http://synapsedu.info/wp-content/uploads/2014/05/Vertical-large.jpg", "http://synapsedu.info/wp-content/uploads/2014/05/imag56es.jpg", "http://synapsedu.info/wp-content/uploads/2014/05/girl1.jpg", "http://synapsedu.info/wp-content/uploads/2014/05/tom_jerry.jpg", "http://synapsedu.info/wp-content/uploads/2014/05/cover_pic1.png", "http://synapsedu.info/wp-content/uploads/2014/06/Tulips.jpg", "http://synapsedu.info/wp-content/uploads/2014/06/Jellyfish.jpg", "http://synapsedu.info/wp-content/uploads/2014/06/Koala.jpg", "http://synapsedu.info/wp-content/uploads/2014/06/Lighthouse.jpg"]
+          };
+          return d.resolve(listOfFiles);
+        });
       };
-      return listOfFiles;
+      return $.when(runFunc()).done(function() {
+        return console.log('getListOfMediaFilesFromServer done');
+      }).fail(_.failureHandler);
     }
   });
 });
--- wp-content/themes/walnut/walnut/dev/source/apps/app-sync/app-sync-operations/app-sync-media.coffee
+++ wp-content/themes/walnut/walnut/dev/source/apps/app-sync/app-sync-operations/app-sync-media.coffee
@@ -9,44 +9,76 @@ define ['underscore','jquery'], ( _ , $) ->
 
 			localFiles = _.getListOfMediaFilesFromLocalDirectory()
 			localFiles.done (local_entries)->
+
+				console.log 'local_entries'
+				console.log local_entries
 				
 				filesOnServer = _.getListOfMediaFilesFromServer()
+				filesOnServer.done (files_on_server)->
 				
-				filesToBeDownloaded = _.compareFiles(local_entries, filesOnServer)
+					filesToBeDownloaded = _.compareFiles(local_entries, files_on_server)
+					filesToBeDownloaded.done (files_to_be_downloaded)->
 
-				_.each filesToBeDownloaded, (file, i)->
+						$('#syncMediaSuccess').css("display","block").text("Downloading files...")
 
-					directoryPath = file.substr(file.indexOf("uploads/"))
-				
-					directoryStructure = _.createDirectoryStructure(directoryPath)
-					directoryStructure.done ->
-						console.log 'Directory successfully created'
+						_.each files_to_be_downloaded, (file, i)->
 
-					# ft = new FileTransfer()
-					# ft.download(uri, filePath 
-					# 	,(file)->
-					# 		$('#syncMediaSuccess').css("display","block").text("files to be downloaded are ")
+							directoryPath = file.substr(file.indexOf("uploads/"))
 
-					# 	,_.fileTransferErrorHandler, true)
+							fileName = file.substr(file.lastIndexOf('/') + 1)
+
+							uri = encodeURI file
+
+							localPath = _.getSynapseImagesDirectoryPath() + directoryPath
+						
+							directoryStructure = _.createDirectoryStructure directoryPath
+							directoryStructure.done ->
+
+								fileTransfer = new FileTransfer()
+								fileTransfer.download(uri, localPath 
+									,(file)->
+										$('#syncMediaSuccess').css("display","block")
+										.text("Downloaded file "+fileName)
+
+									,(error)->
+										$('#syncMediaSuccess').css("display","none")
+										
+										$('#syncMediaError').css("display","block")
+										.text("An error occurred during file download.")
+
+
+									, true)
 
 
 		
 		compareFiles :(localEntries, serverEntries) ->
 
-			filesTobeDownloaded = []
+			runFunc = ->
+				$.Deferred (d)->
 
-			_.each serverEntries.fileImg, (serverFile, i)->
-				fileName = serverFile[i].substr(serverFile[i].lastIndexOf('/') + 1)
-				if localEntries.indexOf(fileName) == -1
-					filesTobeDownloaded.push serverFile[i]
+					if localEntries.length is 0
+						d.resolve serverEntries.fileImg
 
-			filesTobeDownloaded
+					else
+						filesTobeDownloaded = []
+
+						_.each serverEntries.fileImg, (serverFile, i)->
+							fileName = serverFile[i].substr(serverFile[i].lastIndexOf('/') + 1)
+							if localEntries.indexOf(fileName) == -1
+								filesTobeDownloaded.push serverFile[i]
+
+						d.resolve filesTobeDownloaded
+
+
+			$.when(runFunc()).done ->
+				console.log 'compareFiles done'
+			.fail _.failureHandler
 
 
 		
 		getListOfMediaFilesFromLocalDirectory : ->
 
-			listOfPresentFilesInDirectory = ->
+			runFunc = ->
 				$.Deferred (d)->
 					fullDirectoryEntry = []
 
@@ -60,10 +92,8 @@ define ['underscore','jquery'], ( _ , $) ->
 									reader = directoryEnrty.createReader()
 									reader.readEntries ((directoryEnrty)->
 										for i in [0..directoryEnrty.length-1] by 1
-											console.log directoryEnrty[i].name+' dir? '+directoryEnrty[i].isDirectory
-											console.log "your path is "+directoryEnrty[i].toURL()
 											fullDirectoryEntry[i] = directoryEnrty[i].name
-										console.log "value is "+fullDirectoryEntry
+
 										d.resolve fullDirectoryEntry
 
 									)
@@ -73,25 +103,32 @@ define ['underscore','jquery'], ( _ , $) ->
 
 						, _.fileSystemErrorHandler)
 
-			$.when(listOfPresentFilesInDirectory()).done ->
-				console.log 'Got list of all files present in the directory'
+			$.when(runFunc()).done ->
+				console.log 'Got list of all files present in the local directory'
 			.fail _.failureHandler
 		
 				
 
 		getListOfMediaFilesFromServer:->
-			listOfFiles = []
-			listOfFiles = 
-				fileImg : [ "http://synapsedu.info/wp-content/uploads/videos/oceans-clip.mp4"
-							"http://synapsedu.info/wp-content/uploads/2014/05/tux.png"
-							"http://synapsedu.info/wp-content/uploads/2014/05/Vertical-large.jpg"
-							"http://synapsedu.info/wp-content/uploads/2014/05/imag56es.jpg"
-							"http://synapsedu.info/wp-content/uploads/2014/05/girl1.jpg"
-							"http://synapsedu.info/wp-content/uploads/2014/05/tom_jerry.jpg"
-							"http://synapsedu.info/wp-content/uploads/2014/05/cover_pic1.png"
-							"http://synapsedu.info/wp-content/uploads/2014/06/Tulips.jpg"
-							"http://synapsedu.info/wp-content/uploads/2014/06/Jellyfish.jpg"
-							"http://synapsedu.info/wp-content/uploads/2014/06/Koala.jpg"
-							"http://synapsedu.info/wp-content/uploads/2014/06/Lighthouse.jpg"]
+
+			runFunc = ->
+				$.Deferred (d)->
+					listOfFiles = []
+					listOfFiles = 
+						fileImg : [ "http://synapsedu.info/wp-content/uploads/videos/oceans-clip.mp4"
+									"http://synapsedu.info/wp-content/uploads/2014/05/tux.png"
+									"http://synapsedu.info/wp-content/uploads/2014/05/Vertical-large.jpg"
+									"http://synapsedu.info/wp-content/uploads/2014/05/imag56es.jpg"
+									"http://synapsedu.info/wp-content/uploads/2014/05/girl1.jpg"
+									"http://synapsedu.info/wp-content/uploads/2014/05/tom_jerry.jpg"
+									"http://synapsedu.info/wp-content/uploads/2014/05/cover_pic1.png"
+									"http://synapsedu.info/wp-content/uploads/2014/06/Tulips.jpg"
+									"http://synapsedu.info/wp-content/uploads/2014/06/Jellyfish.jpg"
+									"http://synapsedu.info/wp-content/uploads/2014/06/Koala.jpg"
+									"http://synapsedu.info/wp-content/uploads/2014/06/Lighthouse.jpg"]
 				
-			listOfFiles
+					d.resolve listOfFiles
+
+			$.when(runFunc()).done ->
+				console.log 'getListOfMediaFilesFromServer done'
+			.fail _.failureHandler
