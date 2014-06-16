8c34acbcd46312e667e5cd67e7cc883288927b8a
Jeromie Fernandes <jeromie@ajency.in>
2014-06-16 12:30:10 +0530

    student selection color changes, take quiz markup

--- wp-content/themes/walnut/walnut/dev/js/apps/teachers-dashboard/teacher-teaching-module/student-list/student-list-views.js
+++ wp-content/themes/walnut/walnut/dev/js/apps/teachers-dashboard/teacher-teaching-module/student-list/student-list-views.js
@@ -14,7 +14,7 @@ define(['app'], function(App) {
 
       StudentsItemView.prototype.className = 'col-sm-3 m-b-20';
 
-      StudentsItemView.prototype.template = '<div class="row single tiles white no-margin" data-id="{{ID}}"> <div class="col-md-8 col-xs-8 no-padding"> <div class="text-center"> <h4 class="text-primary no-margin p-t-20 p-b-20 p-l-5 p-r-5"><span class="semi-bold">{{display_name}}</span> </h4> <div class="clearfix"></div> </div> </div> <div class="col-md-4 col-xs-4 no-padding"> <div class="tiles default active"> <div class="user-profile-pic text-left m-t-0 p-t-10"> <img data-src-retina="{{profile_pic}}" data-src="{{profile_pic}}" src="{{profile_pic}}" alt=""> </div> <div class="bold text-white text-center p-t-5 p-b-5"><i class="fa fa-minus-circle"></i></div> </div> </div> </div>';
+      StudentsItemView.prototype.template = '<div class="row single tiles white no-margin" data-id="{{ID}}"> <div class="col-md-8 col-xs-8 no-padding"> <div class="text-center"> <h4 class="text-primary no-margin p-t-20 p-b-20 p-l-5 p-r-5"><span class="semi-bold">{{display_name}}</span> </h4> <div class="clearfix"></div> </div> </div> <div class="col-md-4 col-xs-4 no-padding"> <div class="tiles unselected active"> <div class="user-profile-pic text-left m-t-0 p-t-10"> <img data-src-retina="{{profile_pic}}" data-src="{{profile_pic}}" src="{{profile_pic}}" alt=""> </div> <div class="bold text-white text-center p-t-5 p-b-5"><i class="fa fa-minus-circle"></i></div> </div> </div> </div>';
 
       return StudentsItemView;
 
@@ -110,7 +110,7 @@ define(['app'], function(App) {
       };
 
       StudentsList.prototype.markAsCorrectAnswer = function(student) {
-        return $(student).removeClass('selected').find('.default').removeClass('default').addClass('green').find('i').removeClass('fa-minus-circle').addClass('fa-check-circle');
+        return $(student).removeClass('selected').find('.unselected').removeClass('unselected').addClass('blue').find('i').removeClass('fa-minus-circle').addClass('fa-check-circle');
       };
 
       StudentsList.prototype.removeFromCorrectList = function() {
@@ -119,7 +119,7 @@ define(['app'], function(App) {
         for (_i = 0, _len = selectedStudents.length; _i < _len; _i++) {
           student = selectedStudents[_i];
           this.correctAnswers = _.without(this.correctAnswers, parseInt($(student).attr('data-id')));
-          $(student).removeClass('selected').find('.green').removeClass('green').addClass('default').find('i').removeClass('fa-check-circle').addClass('fa-minus-circle');
+          $(student).removeClass('selected').find('.blue').removeClass('blue').addClass('unselected').find('i').removeClass('fa-check-circle').addClass('fa-minus-circle');
         }
         return this.trigger("save:question:response", this.correctAnswers);
       };
