b11bba784f016f8704e403db7bdf0aee1a5c6882
Supresh Colaco <supresh@ajency.in>
2014-07-22 14:28:12 +0530

    student csv import from admin creating import logs

--- wp-content/themes/walnut/modules/school/wpadmin-custom-menus.php
+++ wp-content/themes/walnut/modules/school/wpadmin-custom-menus.php
@@ -46,6 +46,17 @@ function school_csv_import_options() {
 
     if ( !empty($import_status) &&  $import_status['success']) { ?>
         <div id="message" class="updated"><p><strong><?php echo $import_status['msg']; ?></strong></p></div>
+        <table>
+            <tr>
+                <td>Students Added:</td>   <td><?php echo $import_status['added']; ?></td> <td><?php echo $import_status['added_report']; ?></td> 
+            </tr>
+            <tr>
+                <td>Students Updated:</td> <td><?php echo $import_status['updated']; ?></td> <td><?php echo $import_status['updated_report']; ?></td> 
+            </tr>
+            <tr>
+                <td>Students Failed:</td> <td><?php echo $import_status['failed']; ?></td> <td><?php echo $import_status['failed_report']; ?></td> 
+            </tr>
+        </table>
     <?php } ?>
         
     <form action="" method="post" enctype="multipart/form-data">
@@ -58,10 +69,15 @@ function school_csv_import_options() {
     <?php 
 }
 
+/*
+ * function to import user provided student csv file
+ */
 function import_student_csv($file_path){
     global $wpdb;
+    $updated_records = $new_records = $failed_records = array();
     // get json from parsed csv data
     $csv_json = parseCSV($file_path['tmp_name']);
+    
     $csvData = json_decode($csv_json);
     
     $student_csv_headers = array('NAME',
@@ -74,21 +90,24 @@ function import_student_csv($file_path){
                                  'PARENT_MOBILE_1',
                                  'PARENT_EMAIL_ID_2',
                                  'PARENT_MOBILE_2');
-    //print_r($csvData);
-    
-    //While there is an entry in the CSV data
+
     $i=1;
+    $update_count = $insert_count = $failed_count = 0;
     
     if($student_csv_headers !== $csvData[0] ){
             $imp_status = array('success'=>false,'msg'=>'Column Headers incorrect.');
             return $imp_status;
     }
-    
+ 
+  //While there is an entry in the CSV data    
     while ($i <= count($csvData)-1 ) {
         
-        if(count($csvData[$i]) !== 10){
-            $imp_status = array('success'=>false,'msg'=>'Column Count incorrect.');
-            return $imp_status;
+        if( count($csvData[$i]) !== 10  || $csvData[$i][6] == '' || !is_email($csvData[$i][6]) || $csvData[$i][7] == '' ){
+            $csvData[$i][] = get_failed_status($csvData[$i]);
+            $failed_records[] = $csvData[$i];
+            $failed_count++;
+            $i++;
+            continue;
         }
         
 	$user_table = $wpdb->prefix ."users";
@@ -114,13 +133,15 @@ function import_student_csv($file_path){
 			$user_id = $userExists->ID; 
 			$userdata = array(
 				'ID'            => $user_id,
-				'user_pass'     => $user_pass,
+				'user_pass'     => wp_hash_password($user_pass),
 				'user_login'    => $user_login,
 				'user_email'    => $user_email
 
 			);
 	
                         wp_insert_user( $userdata ) ;
+                        $updated_records[] = $csvData[$i];
+                        $update_count++;
 		}
 		else{
 			$userdata = array(
@@ -133,28 +154,42 @@ function import_student_csv($file_path){
 			$user_id = wp_insert_user( $userdata ) ;
                         
                         if( is_wp_error( $user_id ) ){
-                                $imp_status = array('success'=>false,'msg'=>'User Could not be created at row:'.$i);
-                                return $imp_status;
+                            $csvData[$i][] = get_failed_status($csvData[$i]);
+                            $failed_records[] = $csvData[$i];
+                            $failed_count++;
+                            $i++;
+                            continue;
                         }
+                        
+                        $new_records[] = $csvData[$i];
+                        $insert_count++;
 
 		}
 		
-		//Insert/Update user meta table	
-		update_user_meta( $user_id, 'student_division', $meta_value_division );
-		update_user_meta( $user_id, 'roll_no', $meta_value_rollno);
-                update_user_meta( $user_id, 'parent_phone1', $parent_phone1 );
-		update_user_meta( $user_id, 'parent_phone2', $parent_phone2);
-		//add student to blog
-		add_user_to_blog( $blogId, $user_id, $role );
+                if(! is_wp_error( $user_id ) ){
+                    //Insert/Update user meta table	
+                    update_user_meta( $user_id, 'student_division', $meta_value_division );
+                    update_user_meta( $user_id, 'roll_no', $meta_value_rollno);
+                    update_user_meta( $user_id, 'parent_phone1', $parent_phone1 );
+                    update_user_meta( $user_id, 'parent_phone2', $parent_phone2);
+
+                    //add student to blog
+                    add_user_to_blog( $blogId, $user_id, $role );
+
+                    $parent_emails = array('parent_email1' =>$parent_email1,'parent_email2' =>$parent_email2);
+                    add_update_student_parents($user_id,$parent_emails);
+                }
                 
-                $parent_emails = array('parent_email1' =>$parent_email1,'parent_email2' =>$parent_email2);
-                add_update_student_parents($user_id,$parent_emails);
                 
         $i++;
 
     }
     
-    $imp_status = array('success'=>true,'msg'=>'Students imported.');
+    $import_log_filepaths = get_import_log_status_files($student_csv_headers,$updated_records,$new_records,$failed_records);
+    $imp_status = array('success'=>true,'msg'=>'Import Completed.','updated'=>$update_count,'added'=>$insert_count,
+                        'failed'=>$failed_count,'added_report'=>$import_log_filepaths['added_records_log'],
+                        'updated_report'=>$import_log_filepaths['updated_records_log'],
+                        'failed_report'=>$import_log_filepaths['failed_records_log']);
     return $imp_status;
 }
 
@@ -176,7 +211,8 @@ function add_update_student_parents($user_id,$parent_emails = array()){
                            )
                          );
             add_user_to_blog(get_current_blog_id(), $new_parent_id, 'parent' );
-            wp_new_user_notification($new_parent_id, $password);
+            //avoiding sending notification incase of bulk new parent users
+            //wp_new_user_notification($new_parent_id, $password);
 
             update_user_meta( $user_id, $key, $email_address  );
             update_user_meta( $new_parent_id, 'parent_of', $user_id );
@@ -184,4 +220,103 @@ function add_update_student_parents($user_id,$parent_emails = array()){
         }
         
     }
+}
+
+function get_import_log_status_files($csv_headers,$updated,$new,$failed){
+    $ret_data = array('updated_records_log' => '',
+                      'added_records_log' => '',
+                      'failed_records_log' => '');
+     
+    clear_csv_import_logs();
+    
+    $uploads_dir = wp_upload_dir();
+
+    $upload_directory = str_replace('/images', '', $uploads_dir['basedir']);
+    $upload_url = str_replace('/images', '', $uploads_dir['baseurl']);
+    
+    if(!file_exists($upload_directory.'/tmp/'))
+        mkdir($upload_directory.'/tmp',0755);
+
+    if(!file_exists($upload_directory.'/tmp/csv_import_logs'))
+        mkdir($upload_directory.'/tmp/csv_import_logs',0755);
+
+    if(count($updated) > 0){
+        $random= rand(9999,99999);
+        $log_path= '/tmp/csv_import_logs/updated-'.$random.date('Ymdhis').'.csv';
+        $csv_handler = fopen($upload_directory.$log_path, 'w');
+        
+        fputcsv($csv_handler, $csv_headers);
+        foreach ($updated as $fields) 
+        {
+            fputcsv($csv_handler, $fields);
+        }
+
+        fclose($csv_handler);    
+        
+        $ret_data['updated_records_log'] = "<a href='".$upload_url.$log_path."' target='_blank'>View Records</a>";
+    }
+    
+    if(count($new) > 0){
+        $random= rand(9999,99999);
+        $log_path= '/tmp/csv_import_logs/added-'.$random.date('Ymdhis').'.csv';
+        $csv_handler = fopen($upload_directory.$log_path, 'w');
+        
+        fputcsv($csv_handler, $csv_headers);
+        foreach ($new as $fields) 
+        {
+            fputcsv($csv_handler, $fields);
+        }
+
+        fclose($csv_handler);  
+        
+        $ret_data['added_records_log'] = "<a href='".$upload_url.$log_path."' target='_blank'>View Records</a>";
+    }
+    
+    if(count($failed) > 0){
+        $random= rand(9999,99999);
+        $log_path= '/tmp/csv_import_logs/failed-'.$random.date('Ymdhis').'.csv';
+        $csv_handler = fopen($upload_directory.$log_path, 'w');
+        $tmp_csv_headers = $csv_headers;
+        $tmp_csv_headers[] = 'REASON';
+        fputcsv($csv_handler, $tmp_csv_headers);
+        foreach ($failed as $fields) 
+        {
+            fputcsv($csv_handler, $fields);
+        }
+
+        fclose($csv_handler);  
+        $ret_data['failed_records_log'] = "<a href='".$upload_url.$log_path."' target='_blank'>View Records</a>";
+    }
+    
+    return $ret_data;
+}
+
+function clear_csv_import_logs(){
+    
+    $uploads_dir = wp_upload_dir();
+    
+    $log_dir_path = str_replace('/images', '', $uploads_dir['path']) . '/tmp/csv_import_logs';
+    if(file_exists($log_dir_path)){
+        $files = glob($log_dir_path.'/*'); // get all file names from csv_import_logs folder
+        foreach($files as $file){ // iterate files
+            if(is_file($file))
+                unlink($file); // delete file
+        }
+    }
+}
+
+function get_failed_status($csvDataRecord){
+
+    if(count($csvDataRecord) !== 10)
+        $status = 'Columns Not Equal';
+    elseif($csvDataRecord[6] == '' )
+         $status = 'Parent Email required';
+    elseif(!is_email($csvDataRecord[6]))
+         $status = 'Invalid Parent Email';
+    elseif($csvDataRecord[7] == '')
+         $status = 'Parent Mobile required';
+    else
+         $status = '-';
+    
+    return $status;
 }
