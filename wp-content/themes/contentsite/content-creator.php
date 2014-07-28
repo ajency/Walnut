9625b7b82944e797c9653e122678702b59b1bb26
Naizy Monteiro <naizy@ajency.in>
2014-07-28 10:58:05 +0530

    version number defined in wp_config

--- wp-content/themes/contentsite/content-creator.php
+++ wp-content/themes/contentsite/content-creator.php
@@ -88,10 +88,7 @@ var _WPNONCE    = '<?php echo wp_create_nonce('media-form');?>';
 CHORUS_OPTIONS= {};
 <?php foreach($chorus_options as $key=>$value){ ?>
 CHORUS_OPTIONS['<?php echo $key?>'] = '<?php echo $value?>';
-<?php }
- $ver = date('YmdHis');
- $VERSION = 1.0;
- ?>
+<?php } ?>
 
 </script>
 
@@ -103,10 +100,10 @@ CHORUS_OPTIONS['<?php echo $key?>'] = '<?php echo $value?>';
 </script>
  <script type="text/javascript" src="<?=get_site_url()?>/wp-content/themes/walnut/walnut/dev/js/plugins/ckeditor/ckeditor.js"></script> 
 <?php if(ENV=='dev') { ?>
-<script type="text/javascript" data-main="<?=get_site_url()?>/wp-content/themes/walnut/walnut/dev/js/content-creator-main.js?ver=<?php echo $ver?>" src="<?=get_site_url()?>/wp-content/themes/walnut/walnut/dev/js/plugins/require.js"></script>
+<script type="text/javascript" data-main="<?=get_site_url()?>/wp-content/themes/walnut/walnut/dev/js/content-creator-main.js?ver=<?php echo DEV_VERSION?>" src="<?=get_site_url()?>/wp-content/themes/walnut/walnut/dev/js/plugins/require.js"></script>
 <?php } else { ?>
 
- <script type="text/javascript"  src="<?=get_site_url()?>/wp-content/themes/walnut/walnut/production/content-creator-main.js?ver=<?php echo $VERSION?>"></script>
+ <script type="text/javascript"  src="<?=get_site_url()?>/wp-content/themes/walnut/walnut/production/content-creator-main.js?ver=<?php echo VERSION?>"></script>
 <?php } ?>
 
 
