93d792cb979ae271f5b74181b211eeb04890a45f
Naizy Monteiro <naizy@ajency.in>
2014-12-09 13:19:32 +0530

    changed to production

--- wp-content/plugins/school-data-sync/school_data_sync.php
+++ wp-content/plugins/school-data-sync/school_data_sync.php
@@ -11,7 +11,7 @@
 //"http:\/\/synapsedu.info\/wp-content\/uploads\/sites\/14\/tmp\/downsync\/csvs-7365920140710064728.zip
 //http://subinsb.com/php-download-extract-zip-archives
 
-define('REMOTE_SERVER_URL','http://synapsedu.info');
+define('REMOTE_SERVER_URL','http://synapselearning.net');
 
 require_once( plugin_dir_path( __FILE__ ) . 'includes/functions.php');
 require_once( plugin_dir_path( __FILE__ ) . 'includes/ajax.php');
