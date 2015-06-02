d5b3599045ac92915a2a8fb6c7d8db08eccab6b9
Naizy Monteiro <naizy@ajency.in>
2015-06-02 17:23:39 +0530

    basic outline for the form & submission done

--- wp-content/themes/walnut/modules/content-pieces/content_pieces_import.php
+++ wp-content/themes/walnut/modules/content-pieces/content_pieces_import.php
@@ -1,8 +1,28 @@
 <?php
 
 function get_content_pieces_import_page_html_data(){
-    
-    return "<h2>Content Pieces Import</h2>";
-    
-    
+   
+	if(isset($_POST['submit']) && !empty($_POST)){
+		check_admin_referer( 'content-pieces-import' );
+		import_content_piece($_POST['type']);
+	}
+	?>
+   <h2>Content Pieces Import</h2>
+   
+   <form method="post" action="">
+   		
+   		<label>MCQ</label>: 
+   		<input type="file">
+   		<?php wp_nonce_field( 'content-pieces-import' );?>
+   		<input name="type" type="hidden" value="mcq">
+   		<input name="submit" type="submit">
+   		
+   </form>
+   
+   <?php
+}
+
+function import_content_piece($type, $file){
+	echo $type;
+	echo "test import"; exit;
 }
