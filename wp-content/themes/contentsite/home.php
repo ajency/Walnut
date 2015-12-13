d6210e3e76f96b2f3ea8cfed66d39fba5902df0b
kapil-ajency <kapil@ajency.in>
2015-12-13 16:25:36 +0530

    temp

--- wp-content/themes/contentsite/home.php
+++ wp-content/themes/contentsite/home.php
@@ -29,8 +29,11 @@
     <link href="./wp-content/themes/walnut/walnut/dev/css/font-awesome.css" rel="stylesheet" type="text/css">
     <link href="./wp-content/themes/walnut/walnut/dev/css/animate.min.css" rel="stylesheet" type="text/css">
     <link href="./wp-content/themes/walnut/walnut/dev/css/bootstrap-spinedit.css" rel="stylesheet" type="text/css">
+    <link href="./wp-content/themes/walnut/walnut/dev//css/jquery.resizableColumns.css" rel="stylesheet" media="screen"/>
 
-
+    <!-- BEGIN BOOK BLOCK -->
+    <link href="./wp-content/themes/walnut/walnut/dev/css/bookBlock/custom.css" rel="stylesheet" type="text/css">
+    <link href="./wp-content/themes/walnut/walnut/dev/css/bookBlock/bookblock.css" rel="stylesheet" type="text/css">
     <!-- END CORE CSS FRAMEWORK -->
 
     <!-- BEGIN CSS TEMPLATE -->
@@ -38,13 +41,12 @@
     <link href="./wp-content/themes/walnut/walnut/dev/css/responsive.css" rel="stylesheet" type="text/css">
     <link href="./wp-content/themes/walnut/walnut/dev/css/custom-icon-set.css" rel="stylesheet" type="text/css">
 
-    <!-- BEGIN BOOK BLOCK -->
-    <link href="./wp-content/themes/walnut/walnut/dev/css/bookBlock/custom.css" rel="stylesheet" type="text/css">
-    <link href="./wp-content/themes/walnut/walnut/dev/css/bookBlock/bookblock.css" rel="stylesheet" type="text/css">
-    <!-- END BOOK BLOCK -->
-    
     <link href="./wp-content/themes/walnut/walnut/dev/css/custom.css" rel="stylesheet" type="text/css"/>
+
 </head>
+<?php 
+//require_once('header-student.php');
+?>
 <body class="">
 <div id="site_main_container">
     <div>
@@ -55,12 +57,14 @@
 
             <div class="page-content">
                 <div class="content">
-                    <?php if(!is_multisite()) :?>
+                     <?php if(!is_multisite()) :?>
                         <?php if(!school_is_syncd()) : ?>
-                        <p>You may not see any data if you have not synced. If you are a school admin then go to your link <a href="<?php echo admin_url().'options-general.php?page=school_data_sync' ?>">School Data Sync</a> <p> 
+                    <p align="center">You may not see any data if you have not synced.</p> 
+                    <p align="center">Sync data from the drop down on the top right corner after logging in<p> 
+                    <iframe style="position:absolute;top:-5000px" src="<?php  site_url() ?>/wp-admin/options-permalink.php"></iframe>    
                         <?php endif; ?>
                     <?php endif;?>
-                            
+                                          
                     <div id="login-region"></div>
                     <div id="breadcrumb-region"></div>
                     <div id="main-content-region"></div>
@@ -72,7 +76,55 @@
     <div id="dialog-region">
     </div>
 </div>
+<?php global $class_ids;
+global $chorus_options; 
 
+?>
+<script>
+    AJAXURL = '<?php echo admin_url("admin-ajax.php") ?>';
+    SITEURL = '<?php echo site_url() ?>';
+    THEMEURL = '<?php echo get_template_directory_uri()?>';
+
+    <?php if(is_multisite()){?>
+        IS_STANDALONE_SITE = false
+    <?php }
+    else { ?>
+        IS_STANDALONE_SITE = true
+    <?php }?>
+
+    <?php getLoggedInUserModel()?>
+
+    CLASS_LABEL = {};
+    <?php foreach($class_ids as $class){ ?>
+    CLASS_LABEL[<?php echo $class['id']?>] = '<?php echo $class['label']?>';
+    <?php } ?>
+
+    CHORUS_OPTIONS = {};
+    <?php foreach($chorus_options as $key=>$value){ ?>
+    CHORUS_OPTIONS['<?php echo $key?>'] = '<?php echo $value?>';
+    <?php } ?>
+
+</script>
+<script type="text/javascript" src="./wp-content/themes/walnut/walnut/dev/js/plugins/pace.js"></script>
+<script>
+    Pace.on('hide', function () {
+        document.getElementById("site_main_container").setAttribute('class', 'showAll');
+    })
+</script>
+<?php
+
+if (ENV == 'dev') {
+    ?>
+    <script type="text/javascript"
+            data-main="./wp-content/themes/walnut/walnut/dev/js/walnut-main.js?ver=<?php echo DEV_VERSION ?>"
+            src="./wp-content/themes/walnut/walnut/dev/js/plugins/require.js"></script>
+<?php
+} else {
+
+?>
+    <script type="text/javascript"
+            src="./wp-content/themes/walnut/walnut/production/walnut-main.js?ver=<?php echo VERSION ?>"></script>
+<?php } ?>
 
 </body>
 </html>
