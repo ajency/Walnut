8ce01faec9c1103a50919fc317e9f938e40adf30
kapil-ajency <kapil@ajency.in>
2015-12-14 10:16:11 +0530

    event prevent default problem test

--- wp-content/themes/walnut/walnut/dev/js/apps/login/logincontroller.js
+++ wp-content/themes/walnut/walnut/dev/js/apps/login/logincontroller.js
@@ -104,8 +104,9 @@ define(['app', 'controllers/region-controller', 'text!apps/login/templates/login
 
       LoginView.prototype.submitLogin = function(e) {
         var data;
-        e.defaultPrevented;
+        e.preventDefault();
         if (this.$el.find('form').valid()) {
+          alert();
           this.$el.find('#checking_login').remove();
           this.$el.find('#login-submit').append('<i id="checking_login" class="fa fa-spinner fa fa-1x fa-spin"></i>');
           data = Backbone.Syphon.serialize(this);
