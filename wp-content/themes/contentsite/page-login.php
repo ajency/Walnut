ba316768136adf09ff1d6c619fc6cbde25269d4b
kapil-ajency <kapil@ajency.in>
2015-12-15 11:12:51 +0530

    design changes

--- wp-content/themes/contentsite/page-login.php
+++ wp-content/themes/contentsite/page-login.php
@@ -55,85 +55,105 @@
          ?>
    </head>
    <body>
-    <header>
-        <nav class="navbar navbar-default wallnut-nav">
-            <div class="container-fluid signin-header">
-                <div class="logo">
-                    <img src="<?php echo STUDENT_ASSET_PATH ;?>images/walnut-logo.png" class="img-responsive center-block" alt="">
-                </div>
-                <div class="slogan">
-                    Tough Outside. Smart Inside
-                </div>
-            </div>
-        </nav>
-    </header>   
-      <!-- Dashboard-Header -->
-      <div class="signin-card">
-         <div class="signin-wrapper">
-            <div class="container-fluid">
-               <form action="">
-                  <div class="row">
-                     <div class="col-sm-6 col-sm-offset-3 col-lg-6 col-lg-offset-3 wrapped">
-                        <div class="row text-center">
-                           <div class="col-sm-6 col-lg-6 border-rl">
-                              <div class="school-details">
-                                 <div class="profile-pic">
-                                    <img src="<?php echo STUDENT_ASSET_PATH ;?>images/walnut-logo.png" alt="" class="img-responsive center-block">
-                                 </div>
-                                 <h5><a href="http://walnut.synapsedu.info">At Shivane</a></h5>
-                              </div>
-                           </div>
-                           <div class="col-sm-6 col-lg-6">
-                              <div class="school-details">
-                                 <div class="profile-pic">
-                                    <img src="<?php echo STUDENT_ASSET_PATH ;?>images/walnut-logo.png" alt="" class="img-responsive center-block">
-                                 </div>
-                                 <h5><a href="">At Fursungi</a></h5>
-                              </div>
-                           </div>
+                <header>
+                    <nav class="navbar navbar-default wallnut-nav navbar-fixed-top">
+                        <div class="container-fluid signin-header">
+                            <div class="logo">
+                                <img src="<?php echo STUDENT_ASSET_PATH ;?>images/walnut-logo.png" class="img-responsive center-block" alt="">
+                            </div>
+                            <div class="slogan">
+                                Tough Outside. Smart Inside
+                            </div>
                         </div>
-                        <hr>
-                        <!--center>Sign In/ Sign Up for Open School</center-->
-                        <div class="row text-center">
-                           <div class="col-sm-6 col-lg-6">
-                              <div class="social-signup">
-                                 <div class="the_champ_login_container">
-                                    <ul class="the_champ_login_ul">
-                                       <i id="theChampGoogleButton" class="fa fa-google-plus btn g-plus " alt="Login with Google" title="Login with Google" onclick="theChampInitiateLogin(this)" >
-                                       Sign in with google
-                                       </i>
-                                    </ul>
-                                 </div>
-                              </div>
-                           </div>
-                           <div class="col-sm-6 col-lg-6">
-                              <div class="social-signup">
-                                 <div class="the_champ_login_container">
-                                    <ul class="the_champ_login_ul">
-                                       <i id="theChampFacebookButton" class="fa fa-facebook btn fb " alt="Login with Facebook" title="Login with Facebook" onclick="theChampInitiateLogin(this)" >
-                                       Sign in with facebook
-                                       </i>
-                                    </ul>
-                                 </div>
-                              </div>
-                           </div>
+                    </nav>
+                </header>
+
+                <!-- Signin/Signup -->
+                <div class="signin-card">
+                    <div class="signin-wrapper">
+                        <div class="container-fluid">
+                            <form action="">
+                                <div class="row">
+                                    <div class="col-sm-6 col-sm-offset-3 col-lg-6 col-lg-offset-3 wrapped">
+                                        <div class="row text-center">
+                                            <div class="col-md-12 reg-login">
+                                                <h4>Welcome to Synapse Learning Institute</h4>
+                                                <h5>Signin/Signup into your account</h5>
+                                            </div>
+                                        </div>                                    
+                                        <div class="row text-center">
+                                            <div class="col-sm-6 col-lg-6 border-rl">
+                                                <div class="school-details">
+                                                    <div class="profile-pic">
+                                                        <img src="<?php echo STUDENT_ASSET_PATH ;?>images/shivane.png" alt="" class="img-responsive center-block">
+                                                    </div>                                              
+                                                    <h5><a href="http://walnut.synapsedu.info">Login at Shivane</a></h5>
+                                                </div>
+                                            </div>
+                                            <div class="col-sm-6 col-lg-6">
+                                                <div class="school-details">
+                                                    <div class="profile-pic">
+                                                          <img src="<?php echo STUDENT_ASSET_PATH ;?>images/fursungi.png" alt="" class="img-responsive center-block">
+                                                    </div>   
+                                                    <h5><a href="">Login at Fursungi</a></h5>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <hr>
+                                        <div class="row text-center">
+                                            <div class="col-md-12 reg-login">
+                                                <h4>Come join the Synapse Learning Universal Community</h4>
+                                                <h5>Lets start up your account here</h5>
+                                            </div>
+                                        </div>                                          
+                                        <div class="row text-center">
+                                            <div class="col-sm-6 col-lg-6 border-rl">
+                                              <div class="social-signup">
+                                                 <div class="the_champ_login_container">
+                                                    <ul class="the_champ_login_ul">
+                                                       <i id="theChampGoogleButton" class="fa fa-google-plus btn g-plus " alt="Login with Google" title="Login with Google" onclick="theChampInitiateLogin(this)" >
+                                                       Sign in with google
+                                                       </i>
+                                                    </ul>
+                                                 </div>
+                                              </div>
+                                            </div>
+                                            <div class="col-sm-6 col-lg-6">
+                                              <div class="social-signup">
+                                                 <div class="the_champ_login_container">
+                                                    <ul class="the_champ_login_ul">
+                                                       <i id="theChampFacebookButton" class="fa fa-facebook btn fb " alt="Login with Facebook" title="Login with Facebook" onclick="theChampInitiateLogin(this)" >
+                                                       Sign in with facebook
+                                                       </i>
+                                                    </ul>
+                                                 </div>
+                                              </div>
+                                            </div>
+                                        </div>
+                                        <!-- <hr>
+                                        <div class="row text-center">
+                                            <div class="col-md-12 sys-login">
+                                                 <h5><a href="">Content Creator Login</a></h5>
+                                                 <h5><a href="">School Login</a></h5>
+                                            </div>
+                                        </div> -->
+                                    </div>
+                                </div>
+                            </form>
+
+                            <!-- Support Emails     -->
+                            <div class="row text-center">
+                                <div class="col-sm-12 col-lg-12">
+                                    <div class="support-email">
+                                        <h5>Support Emails</h5>
+                                        <h5><a href="mailto:walnut@info.com">walnut@info.com</a>/<a href="mailto:helpdesk@walnut.com">helpdesk@walnut.com</a></h5>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                     </div>
-                  </div>
-               </form>
-               <!-- Support Emails     -->
-               <div class="row text-center">
-                  <div class="col-sm-12 col-lg-12">
-                     <div class="support-email">
-                        <h5>Support Emails</h5>
-                        <h5><a href="mailto:walnut@info.com">walnut@info.com</a>/<a href="mailto:helpdesk@walnut.com">helpdesk@walnut.com</a></h5>
-                     </div>
-                  </div>
-               </div>
-            </div>
-         </div>
-      </div>
-     <?php 
+                    </div>
+                </div>    
+        <?php 
          wp_footer();         
          ?>
 
