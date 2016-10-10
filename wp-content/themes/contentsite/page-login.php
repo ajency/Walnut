02ac55e3981125174cd2a657f922883f9ddb5ead
OdoricaAjency <odorica@ajency.in>
2016-10-10 10:52:49 +0530

    social media

--- wp-content/themes/contentsite/page-login.php
+++ wp-content/themes/contentsite/page-login.php
@@ -95,7 +95,7 @@
                                                        <!-- <i id="theChampGoogleButton" class="fa fa-google-plus btn g-plus " alt="Login with Google" title="Login with Google" onclick="theChampInitiateLogin(this)" >
                                                        Sign in with google
                                                        </i> -->
-                                                       <a rel="nofollow" href="javascript:void(0);" title="Connect with Google" class="fa fa-google-plus btn g-plus wp-social-login-provider" data-provider="Google"> Sign in with google</a>
+                                                       <a rel="nofollow" href="javascript:void(0);" title="Connect with Google" class="fa fa-google-plus btn g-plus wp-social-login-provider wp-social-login-provider-google" data-provider="Google"> Sign in with google</a>
                                                     </ul>
                                                  </div>
                                               </div>
@@ -107,7 +107,7 @@
                                                        <!-- <i id="theChampFacebookButton" class="fa fa-facebook btn fb " alt="Login with Facebook" title="Login with Facebook" onclick="theChampInitiateLogin(this)" >
                                                        Sign in with facebook
                                                        </i> -->
-                                                       <a rel="nofollow" href="javascript:void(0);" title="Connect with Facebook" class="fa fa-facebook btn fb wp-social-login-provider" data-provider="Facebook"> Sign in with facebook</a>
+                                                       <a rel="nofollow" href="javascript:void(0);" title="Connect with Facebook" class="fa fa-facebook btn fb wp-social-login-provider wp-social-login-provider-facebook" data-provider="Facebook"> Sign in with facebook</a>
                                                     </ul>
                                                  </div>
                                               </div>
