dd4e1a011c7c6bd39f8df2d4eb855ab605f03e15
OdoricaAjency <odorica@ajency.in>
2017-02-13 13:46:14 +0530

    changes

--- wp-content/themes/walnut/modules/quiz/ExcelGenerate.php
+++ wp-content/themes/walnut/modules/quiz/ExcelGenerate.php
@@ -132,9 +132,9 @@ class ExportExcel {
 
         header('Content-Type: application/vnd.ms-excel;charset=utf-8');
         header('Content-Disposition: attachment;filename="quiz_report'.$quiz_id.'.xls"');
-        header('Cache-Control: max-age=0');
+        //header('Cache-Control: max-age=0');
         // If you're serving to IE 9, then the following may be needed
-        header('Cache-Control: max-age=1');
+        //header('Cache-Control: max-age=1');
         // If you're serving to IE over SSL, then the following may be needed
         header ('Expires: Mon, 26 Jul 1997 05:00:00 GMT'); // Date in the past
         header ('Last-Modified: '.gmdate('D, d M Y H:i:s').' GMT'); // always modified
@@ -145,7 +145,7 @@ class ExportExcel {
         /*$objWriter->save(get_home_path().'wp-content/uploads/q_upload.xls');
         return get_home_path().'wp-content/uploads/q_upload.xls';*/
 
-        ob_clean();
+        //ob_clean();
          $objWriter->save('php://output');
          exit;
         
