17c0910e21f4369145ee9eeca56526603cb225ba
OdoricaAjency <odorica@ajency.in>
2017-02-16 16:14:05 +0530

    excel

--- wp-content/themes/walnut/modules/quiz/ExcelGenerate.php
+++ wp-content/themes/walnut/modules/quiz/ExcelGenerate.php
@@ -4,6 +4,8 @@ class ExportExcel {
 
     public function excel($quiz_id, $division){
 
+        ob_start();
+
         // Create new PHPExcel object
         $objPHPExcel = new PHPExcel();
 
@@ -129,17 +131,17 @@ class ExportExcel {
             
 
         // Redirect output to a client’s web browser (Excel5)
-        ob_start();
+        
         header('Content-Type: application/vnd.ms-excel;charset=utf8');
         header('Content-Disposition: attachment;filename="quiz_report'.$quiz_id.'.xls"');
-        header('Cache-Control: max-age=0');
+        //header('Cache-Control: max-age=0');
         // If you're serving to IE 9, then the following may be needed
-        header('Cache-Control: max-age=1');
+        //header('Cache-Control: max-age=1');
         // If you're serving to IE over SSL, then the following may be needed
-        header ('Expires: Mon, 26 Jul 1997 05:00:00 GMT'); // Date in the past
+        //header ('Expires: Mon, 26 Jul 1997 05:00:00 GMT'); // Date in the past
         header ('Last-Modified: '.gmdate('D, d M Y H:i:s').' GMT'); // always modified
         //header ('Cache-Control: cache, must-revalidate'); // HTTP/1.1
-        //header ('Pragma: public'); // HTTP/1.0
+        header ('Pragma: public'); // HTTP/1.0
         $objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel5');
         //$objWriter->setPreCalculateFormulas(TRUE);
         /*$objWriter->save(get_home_path().'wp-content/uploads/q_upload.xls');
